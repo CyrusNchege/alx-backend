411f0a7787c1462267e452eddea0d58eeba05dc9
CyrusNchege <cyruschegecc@gmail.com>
2023-10-31 11:48:29 +0300

    update basic app and requirements

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
+    """
+    Render the index page with a "Hello, World" message.
+    """
     return render_template('0-index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
